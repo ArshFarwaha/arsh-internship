360fe75ade4953bd6dc5bdc82e87df40d0332b9a
ArshFarwaha <arshdeep.farwaha@gmail.com>
2023-02-21 13:26:58 -0500

    completed author feature

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,12 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Skeleton from "../UI/Skeleton";
 
 const AuthorItems = ({ author }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {author ? (
+          {!loading ? (
+            author ? (
+              author.nftCollection.map((author) => (
+                <div
+                  className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                  key={author.id}
+                >
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link to="">
+                        <img className="lazy" src={author.authorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <Link to={`/item-details/${author.nftId}`}>
+                        <img
+                          src={author.nftImage}
+                          className="lazy nft__item_preview"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to={`/item-details/${author.nftId}`}>
+                        <h4>{author.title}</h4>
+                      </Link>
+                      <div className="nft__item_price">{author.price} ETH</div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{author.likes}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div></div>
+            )
+          ) : author ? (
             author.nftCollection.map((author) => (
               <div
                 className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -15,7 +78,7 @@ const AuthorItems = ({ author }) => {
                 <div className="nft__item">
                   <div className="author_list_pp">
                     <Link to="">
-                      <img className="lazy" src={author.authorImage} alt="" />
+                      <Skeleton width="50px" height="50px" borderRadius="50%" />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
@@ -38,21 +101,21 @@ const AuthorItems = ({ author }) => {
                       </div>
                     </div>
                     <Link to={`/item-details/${author.nftId}`}>
-                      <img
-                        src={author.nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
+                      <Skeleton width="100%" height="200px" />
                     </Link>
                   </div>
                   <div className="nft__item_info">
                     <Link to={`/item-details/${author.nftId}`}>
-                      <h4>{author.title}</h4>
+                      <Skeleton
+                        width="100px"
+                        height="20px"
+                        borderRadius="2px"
+                      />
                     </Link>
-                    <div className="nft__item_price">{author.price} ETH</div>
+                    <br />
+                    <Skeleton width="100px" height="20px" borderRadius="2px" />
                     <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>{author.likes}</span>
+                      <Skeleton width="50px" height="20px" borderRadius="2px" />
                     </div>
                   </div>
                 </div>
